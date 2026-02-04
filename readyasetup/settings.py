c1333654213d7dee629423b77ce30c755d9ecf6b
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-02-05 00:14:05 +0400

    fix:corrected some setings

--- readyasetup/settings.py
+++ readyasetup/settings.py
@@ -36,22 +36,21 @@ ALLOWED_HOSTS = [
     "www.readya-backend.onrender.com",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 load_dotenv(os.path.join(BASE_DIR, '.env')) 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-
-load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Application definition
@@ -158,7 +157,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
