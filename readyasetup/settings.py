6949aabbd289504d3fd3e72d6ed80e496c7066e3
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-02-04 23:23:02 +0400

    fix:host is *

--- readyasetup/settings.py
+++ readyasetup/settings.py
@@ -28,12 +28,7 @@ SECRET_KEY = 'django-insecure-vtu50awcg-ox*ndq&gs_de&a57z%tlvteefw5ahey@dt4u_8=a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "46.225.4.119",
-    "46.225.4.119:80",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 USE_X_FORWARDED_HOST = True
