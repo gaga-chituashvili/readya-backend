89b2628f3760ece84933e510561299670622393f
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-02-06 17:24:59 +0400

    fix:add staticfiles

--- readyaapp/urls.py
+++ readyaapp/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path
-from .views import UploadPDFView, home
+from .views import UploadPDFView, home, stream_mp3
 from .views import UploadPDFView
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
      path('', home, name='home'),
      path("api/upload-pdf/", UploadPDFView.as_view()),
+      path("audio/<uuid:doc_id>/", stream_mp3),
 ]
-
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
 
--- readyaapp/views.py
+++ readyaapp/views.py
@@ -78,3 +78,21 @@ class UploadPDFView(APIView):
             },
             status=201,
         )
+
+
+
+
+from django.http import FileResponse, Http404
+from .models import AudioDocument
+
+def stream_mp3(request, doc_id):
+    try:
+        doc = AudioDocument.objects.get(id=doc_id)
+        if not doc.mp3_file:
+            raise Http404
+        return FileResponse(
+            open(doc.mp3_file.path, "rb"),
+            content_type="audio/mpeg"
+        )
+    except AudioDocument.DoesNotExist:
+        raise Http404
