feceb85debb2a1cbfa23e53b2a8166e40f96ef25
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-02-12 23:55:00 +0400

    feat:add netlify ulr in cors

--- readyasetup/settings.py
+++ readyasetup/settings.py
@@ -63,7 +63,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
-]
+    "https://readya.netlify.app",
+    "https://www.readya.netlify.app",
+    ]
+
+
+
 
 CORS_ALLOW_CREDENTIALS = True
 
