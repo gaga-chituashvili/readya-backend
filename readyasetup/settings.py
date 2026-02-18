d86d691039a3cd8065552f6d4ecf7bab288ab11a
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-02-18 22:28:17 +0400

    fix:add domain url in cors_allowed

--- readyasetup/settings.py
+++ readyasetup/settings.py
@@ -65,6 +65,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://readya.netlify.app",
     "https://www.readya.netlify.app",
+    "https://www.readya.me",
+    "https://readya.me",
     ]
 
 
