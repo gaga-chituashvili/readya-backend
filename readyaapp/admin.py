c42b7ec39fb20dcdb4f255fd7225992e4df5a5c9
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-04-03 17:44:26 +0400

    feat:add SubscriptionPlanAdmin

--- readyaapp/admin.py
+++ readyaapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AudioDocument
+from .models import AudioDocument, SubscriptionPlan
 
 
 
@@ -8,3 +8,11 @@ class AudioDocumentAdmin(admin.ModelAdmin):
     list_display = ("id", "email", "file_type", "status", "created_at")
     list_filter = ("status", "file_type", "created_at")
     search_fields = ("email", "id")
+
+
+
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "duration_days")
+    search_fields = ("name",)
