294a53c3318e9da8473f99eccaa3e0c19b55a24c
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-05-01 22:25:13 +0400

    fix: allow public access to audio stream endpoint

--- readyaapp/view/streammp3_view.py
+++ readyaapp/view/streammp3_view.py
@@ -1,18 +1,18 @@
 from readyaapp.models import AudioDocument
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from django.http import FileResponse, Http404
+from rest_framework.permissions import AllowAny
 @api_view(["GET"])
+@permission_classes([AllowAny]) 
 def stream_mp3(request, doc_id):
     try:
         doc = AudioDocument.objects.get(id=doc_id)
         if not doc.mp3_file:
             raise Http404
+
         return FileResponse(
-            open(doc.mp3_file.path, "rb"),
+            doc.mp3_file.open("rb"),  # 👈 ეს ჯობია ვიდრე open(path)
             content_type="audio/mpeg"
         )
     except AudioDocument.DoesNotExist:
         raise Http404
-    
-
-
